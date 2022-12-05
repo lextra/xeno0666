afa0258c52e35ff395c09bfb887c17e10d969b63
xeno#0666 <112126426+lextra@users.noreply.github.com>
2022-12-05 15:17:51 -0800

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : ['alt @ xeno#0002','legit','/prediction', '/unleak', 'best troll', 'best dev', 'paster', 'very rich'],
+  typeSpeed : 40,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+});
