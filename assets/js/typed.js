b3e69f887c4c424f70a94b052eb189d9bb285c6a
xeno#0666 <112126426+lextra@users.noreply.github.com>
2022-12-27 15:37:07 -0800

    Delete assets/js directory

--- assets/js/typed.js
+++
@@ -1,7 +0,0 @@
-new Typed('#typed',{
-  strings : [,'xeno#0002','/prediction','/varity','immortal','best dev','crypto mastermind','paster',],
-  typeSpeed : 40,
-  delaySpeed : 90,
-  showCursor: false,
-  loop : true
-})
